54bcec108b4a1679987f713c29f590bebdf60735
huoqishi <wangchaohua110@foxmail.com>
2017-12-11 22:20:01 +0800

    docs

--- config/config-prod.js
+++
@@ -1,9 +0,0 @@
-/**
- * the configuration for production
- */
-module.exports = {
-  PORT: 8088,
-  IP: '127.0.0.1',
-  http: 'http://qs.huoqishi.net',
-  https: 'https://qs.huoqishi.net'
-}
--- models/test.js
+++
@@ -1,5 +0,0 @@
-// const db = require('./db')
-const User = require('./User')
-User.findAll().then(users => {
-  console.log(users)
-})
